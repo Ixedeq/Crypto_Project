0da0deb0d46976cd27360bff1578204dbc4027cd
Nate Pardee <109477225+N8Pardee@users.noreply.github.com>
2023-10-17 12:44:26 -0500

    Update MainDriver.cpp
    added decrypting function in main

--- CryptoProject/MainDriver.cpp
+++ CryptoProject/MainDriver.cpp
@@ -2,8 +2,12 @@
 #include <fstream>
 #include <string>
 #include <set>
+#include "Problem8-Detecing AES in ECB mode.cpp"
+
 
 int main() {
+
+    //Detecting AES in ECB
     std::ifstream infile("/mnt/data/8.txt"); // Replace with your file path
     if (!infile.is_open()) {
         std::cerr << "Error opening file!" << std::endl;
@@ -21,5 +25,15 @@ int main() {
     }
 
     infile.close();
+
+    //Decrypting AES-128-ECB with OpenSSL
+    std::string base64_encrypted_data = "your_base64_encrypted_data_here";
+    std::string key = "YELLOW SUBMARINE";
+
+    std::string encrypted_data = decode_base64(base64_encrypted_data);
+    std::string decrypted_data = decrypt_aes_ecb(encrypted_data, key);
+
+    std::cout << "Decrypted data: " << decrypted_data << std::endl;
+    
     return 0;
 }
