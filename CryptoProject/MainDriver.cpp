a3960dac1a36157af683ad7ec7bc5c3bc1dfa6ae
Nate Pardee <109477225+N8Pardee@users.noreply.github.com>
2023-10-17 12:39:10 -0500

    Update MainDriver.cpp
    -Added main from problem 8 12:39 PM

--- CryptoProject/MainDriver.cpp
+++ CryptoProject/MainDriver.cpp
@@ -1,36 +1,25 @@
 #include <iostream>
-#include <bitset>
+#include <fstream>
+#include <string>
+#include <set>
 
-#include "StringToVector.cpp"
-#include "Binary.cpp"
-#include "BinaryToDecimal.cpp"
-#include "Hex_B64.cpp"
-#include "BinaryToHex.cpp"
-#include "XOR.cpp"
-//#include "SingleCharXor.cpp"
-
-int main(){
-    
-    std::string EncryptedHex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    std::string solution = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-   
-    if (Hexb64Converter(EncryptedHex) == solution){
-        std::cout << "True\n";
+int main() {
+    std::ifstream infile("/mnt/data/8.txt"); // Replace with your file path
+    if (!infile.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
     }
-    else{std::cout << "False\n";}
 
-    std::string xor1 = "1c0111001f010100061a024b53535009181c";
-    std::string xor2 = "686974207468652062756c6c277320657965";
-    std::string xorAnswer = "746865206b696420646f6e277420706c6179";
-
-    if (xoring(xor1, xor2) == xorAnswer){
-        std::cout << "True\n";
+    std::string line;
+    size_t line_number = 0;
+    size_t block_size = 32; // AES block size is 16 bytes, but we're working with hex so it's 32 characters
+    while (std::getline(infile, line)) {
+        line_number++;
+        if (has_repeating_blocks(line, block_size)) {
+            std::cout << "Ciphertext on line " << line_number << " is likely encrypted with AES in ECB mode." << std::endl;
+        }
     }
-    else{std::cout << "False\n";}
 
-    std::string SingleXor = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-
-    std::cout << xoring(SingleXor, "f") << "\n";
-    
+    infile.close();
     return 0;
 }
