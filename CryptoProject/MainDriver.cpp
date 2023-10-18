b97d098d7424d26b601f144a1d4195729cc5aad3
Nate Pardee <109477225+N8Pardee@users.noreply.github.com>
2023-10-17 22:23:06 -0500

    Update MainDriver.cpp

--- CryptoProject/MainDriver.cpp
+++ CryptoProject/MainDriver.cpp
@@ -34,7 +34,7 @@ int main(){
     std::cout << xoring(SingleXor, "f") << "\n";
 
     //Problems 4 and 5-Tony
-        string hexInput = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    string hexInput = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     string base64Output = HexToBase64(hexInput);
     cout << "Hex: " << hexInput << endl;
     cout << "Base64: " << base64Output << endl;
