5a32db41d637f1693fc8b6079b1c30f7146fdb42
Nate Pardee <109477225+N8Pardee@users.noreply.github.com>
2023-10-31 14:18:06 -0400

    Update MainDriver.cpp
    Commented out the extra number 5, if we run into issues we can swap out the versions of number 5. Also removed duplicate code for number 7

--- CryptoProject/MainDriver.cpp
+++ CryptoProject/MainDriver.cpp
@@ -41,36 +41,8 @@ int main() {
 
     std::cout << xoring(SingleXor, "f") << "\n";  
 
+ 
      
-     /***********************************
-     Problem 5: Repeating Key XOR main code -zo
-     **********************************/
-     // Example plaintext
-    std::string plaintext =
-        "Burning 'em, if you ain't quick and nimble\n"
-        "I go crazy when I hear a cymbal";
-    
-    std::string key = "ICE";  // Encryption key
-
-    // Encrypt the plaintext
-    std::string ciphertext = repeating_key_xor(plaintext, key);
-    
-    // Expected ciphertext in hexadecimal format
-    std::string expected_hex = 
-        "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
-        "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
-    
-    // Check if the generated ciphertext matches the expected hex
-    if (ciphertext == expected_hex) {
-        std::cout << "Ciphertext matches expected hex." << std::endl;
-    } else {
-        std::cout << "Mismatch detected!" << std::endl;
-    }
-    
-    // Output the plaintext and ciphertext to the console
-    std::cout << "Plaintext:\n" << plaintext << "\n\n";
-    std::cout << "Ciphertext:\n" << ciphertext << "\n";
-
      /************************
           Problem 3 - Zo
      *************************/
@@ -94,7 +66,9 @@ int main() {
     std::cout << "Key: " << bestKey << std::endl;
     std::cout << "Decrypted message: " << bestDecryption << std::endl;
      
-    /**************************
+  
+     
+     /**************************
          Problems 4 and 5-Tony
      **************************/
     string hexInput = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
@@ -180,6 +154,40 @@ int main() {
     std::cout << "Best Key: " << static_cast<int>(best_key) << std::endl;
     std::cout << "Decrypted Message: " << best_decrypted_message << std::endl;
 
+  
+     
+     /***********************************
+     Problem 5: Repeating Key XOR main code -zo
+     **********************************/
+     // Example plaintext
+     /*
+    std::string plaintext =
+        "Burning 'em, if you ain't quick and nimble\n"
+        "I go crazy when I hear a cymbal";
+    
+    std::string key = "ICE";  // Encryption key
+
+    // Encrypt the plaintext
+    std::string ciphertext = repeating_key_xor(plaintext, key);
+    
+    // Expected ciphertext in hexadecimal format
+    std::string expected_hex = 
+        "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
+        "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+    
+    // Check if the generated ciphertext matches the expected hex
+    if (ciphertext == expected_hex) {
+        std::cout << "Ciphertext matches expected hex." << std::endl;
+    } else {
+        std::cout << "Mismatch detected!" << std::endl;
+    }
+    
+    // Output the plaintext and ciphertext to the console
+    std::cout << "Plaintext:\n" << plaintext << "\n\n";
+    std::cout << "Ciphertext:\n" << ciphertext << "\n";
+     */
+     
+    
      
      /*************************************
       Problem 6: decrypting repeating key in main - zo
@@ -193,16 +201,7 @@ int main() {
     
     decryptRepeatingKeyXOR(decodedCiphertext);
 
-     /*************************************************
-     Problem 7: Decrypting AES-128-ECB with OpenSSL-Zo
-    *************************************************/
-    std::string base64_encrypted_data = "your_base64_encrypted_data_here";
-    std::string key = "YELLOW SUBMARINE";
-
-    std::string encrypted_data = decode_base64(base64_encrypted_data);
-    std::string decrypted_data = decrypt_aes_ecb(encrypted_data, key);
-    std::cout << "Decrypted data: " << decrypted_data << std::endl;
-
+     
      
      /*************************************************
      Problem 7: Decrypting AES-128-ECB with OpenSSL-Zo
@@ -215,6 +214,7 @@ int main() {
     std::cout << "Decrypted data: " << decrypted_data << std::endl;
     return 0;
 
+     
 
     /************************************
      Problem 8: Detecting AES in ECB main code-zo
