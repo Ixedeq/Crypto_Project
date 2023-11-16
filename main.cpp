a70952aa490b2b4d400cbd05f0ea95d45037d321
Ixedeq <99107626+Ixedeq@users.noreply.github.com>
2023-11-15 21:53:16 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,15 +3,42 @@
 using namespace std;
 
 int main(){
-    // cout << BinaryToHex(B64Decoder())<< endl;
-    vector<string> oneByte, twoByte;
-    string Test1 = "this is a test";
-    string Test2 = "wokka wokka!!!";
+    ofstream ExternalFile("File.txt"); 
 
-    for(int i = 0; i < Test1.length(); i++){
-        oneByte.insert(oneByte.end(), CharToByte(Test1[i]).begin(), CharToByte(Test1[i]).end());
-        twoByte.insert(twoByte.end(), CharToByte(Test2[i]).begin(), CharToByte(Test2[i]).end());
-    }
-    BreakXor("breakXor.txt");
+
+    // // Problem1
+    // if(HexToB64(HexToBytes(Problem1_Hex)) == Problem1_Solution){
+    //     cout << "Problem1 is working" << endl;
+    // }
+    // //problem2
+    // if(BinaryToHex(Fixed_Xor(HexToBytes(Problem2_Hex1), HexToBytes(Problem2_Hex2))) == Problem2_Solution){
+    //     cout << "Problem2 is working" << endl;
+    // }
+    // //problem3
+    // if(SingleByteXor(HexToBytes(Problem3_Hex)) == Problem3_Solution){
+    //     cout << "Problem3 is working" << endl;
+    // }
+
+    // //problem4
+    string test = DetectingSingleXor(Problem4_FileName);
+   
+    cout << (test) << endl;
+    
+    //cout << Problem4_solution << endl; 
+    //cout << ((test).compare(Problem4_solution))<< endl;    
+    // if(((test).compare(Problem4_solution))==0){
+    //       cout << "Problem4 is working" << endl; 
+    //  }
+    // else{
+    //     cout << "Wtf" << endl;
+    // }
+    //cout<<endl<<"4 Solution"<<DetectingSingleXor(Problem4_FileName)<<endl<<endl;
+
+    // if(BinaryToHex(RepeatingKeyXor(Problem5_Stanza, Problem5_Key)) == Problem5_Answer){
+    //     cout << "problem5 is working" << endl;
+    // }
+
+    // cout << BinaryToHex(B64Decoder())<< endl;
+    //BreakXor("breakXor.txt");
     //cout << fileContent;
 }
